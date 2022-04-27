80c8a927ff0d47aadb298690be1fd424d6fec75b
Saad Ali <59177839+SaadAliHafiz@users.noreply.github.com>
2022-04-27 23:36:57 +0500

    Add files via upload

---
+++ Projects/Supervised Learning/BreastCancer Detection/Model.py.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Model Selection
+
+# #### Logistic Regression
+# #### Decision Tree Classifier
+# #### Random forest classifier
+# #### Support Vector Machine
+
+# ### Logistic Regression
+
+# ### Decision Tree Classifier
+
+# ### Random forest classifier
+
+# ### Support Vector Machine
